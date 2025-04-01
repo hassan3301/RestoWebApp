9d757ae18100715763196101053d352a545c2cc8
hassan3301 <hnishat@hotmail.com>
2025-04-01 12:09:48 -0400

    added blueprint

--- config.py
+++ config.py
@@ -1,14 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://localhost/restodb')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY', 'dev')
+    SECRET_KEY = os.getenv('SECRET_KEY')
 
-# run.py
-from app import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    app.run(debug=True)
